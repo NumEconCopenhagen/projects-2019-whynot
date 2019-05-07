9932033591c618bf7b75abbc9a3b6516aace8567
Nicklas Thorsen <nicklas@Nicklass-MacBook-Pro.local>
2019-05-07 22:36:39 +0200

    consumer problem skeleton

--- modelproject/consumer problem.py
+++ modelproject/consumer problem.py
@@ -17,11 +17,16 @@ q2 = sm.symbols('q_2')
 p1 = sm.symbols('p_1')
 p2 = sm.symbols('p_2')
 rho = sm.symbols('rho')
+mu = sm.symbols('mu')
 I = sm.symbols('I')
 
+#%%
 utility=(q1**rho+q2**rho)**(1/rho)
 utility
 
+#%%
+utility=q1**rho*q2**mu
+utility
 #%%
 budgcons=sm.Eq(p1*q1+p2*q2,I)
 budgcons
@@ -39,4 +44,12 @@ foc
 #%%
 sol = sm.solve(sm.Eq(foc,0),q1)
 sol
-
+#%%
+_sol_func = sm.lambdify((p1,I,rho,mu),sol[0])
+def sol_func(p1,I=10,rho=0.8,mu=0.2):
+    return _sol_func(p1,I,rho,mu)
+    
+# test
+p1_vec = np.array([1.2,3,5,9])
+demand_p1 = sol_func(p1_vec)
+print(demand_p1)
