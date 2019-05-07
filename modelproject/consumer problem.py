016f2b05134243fad67a189b766bb80dc2fb4f6c
Nicklas Thorsen <nicklas@Nicklass-MacBook-Pro.local>
2019-05-07 21:50:08 +0200

    new cons solving

--- modelproject/consumer problem.py
+++ modelproject/consumer problem.py
@@ -5,8 +5,6 @@ from scipy import linalg
 from scipy import optimize
 from scipy import interpolate
 import sympy as sm
-
-%matplotlib inline
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,13 +12,31 @@ from mpl_toolkits.mplot3d import Axes3D
 sm.init_printing(use_unicode=True)
 
 #%%
-def utility(q1,q2,rho):
-    return (q1**rho+q2**rho)**(1/rho)
+q1 = sm.symbols('q_1')
+q2 = sm.symbols('q_2')
+p1 = sm.symbols('p_1')
+p2 = sm.symbols('p_2')
+rho = sm.symbols('rho')
+I = sm.symbols('I')
 
-def budgcons(q1,q2,p1,p2):
-    return q1*p1+q2*p2
+utility=(q1**rho+q2**rho)**(1/rho)
+utility
 
-def consprob(q1,p1,p2,rho,Y):
-    (q1**rho+((Y-q1*p1)/p2)**rho)**(1/rho)
+#%%
+budgcons=sm.Eq(p1*q1+p2*q2,I)
+budgcons
 
+#%%
+q2_iso=sm.solve(budgcons,q2)
+q2_iso
+
+#%%
+util_subs=utility.subs(q2,q2_iso[0])
+util_subs
+#%%
+foc=sm.diff(util_subs,q1)
+foc
+#%%
+sol = sm.solve(sm.Eq(foc,0),q1)
+sol
 
